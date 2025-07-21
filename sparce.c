9aeb83322bda4badcf76733120c7ebaca302033e
RAYAN <rayanjoomy772@gmail.com>
2025-07-21 12:37:45 +0530

    Add files via upload

--- sparce.c
+++ sparce.c
@@ -1,76 +1,120 @@
 #include <stdio.h>
 
-int main() {
-    int r1, c1, n1,r2, c2, n2,i;
-    printf("Enter rows, columns and number of non-zero elements of Matrix 1: ");
-    scanf("%d %d %d", &r1, &c1, &n1);
-    int mat1[3][100];
-    printf("Enter the elements (row column value) of Matrix 1:\n");
-    for(i = 0; i < n1; i++) {
-        scanf("%d %d %d", &mat1[0][i], &mat1[1][i], &mat1[2][i]);
-    }
-    printf("Enter rows, columns and number of non-zero elements of Matrix 2: ");
-    scanf("%d %d %d", &r2, &c2, &n2);
-    int mat2[3][100];
-    printf("Enter the elements (row column value) of Matrix 2:\n");
-    for(i = 0; i < n2; i++) {
-        scanf("%d %d %d", &mat2[0][i], &mat2[1][i], &mat2[2][i]);
-    }
-    if (r1 != r2 || c1 != c2) {
-        printf("Error: Matrices dimensions do not match for addition.\n");
-        return 1;
-    }
-    printf("\nSparse Matrix 1 (row column value):\n");
-    for(i = 0; i < n1; i++) {
-        printf("%d %d %d\n", mat1[0][i], mat1[1][i], mat1[2][i]);
-    }
-    printf("\nSparse Matrix 2 (row column value):\n");
-    for(i = 0; i < n2; i++) {
-        printf("%d %d %d\n", mat2[0][i], mat2[1][i], mat2[2][i]);
-    }
-    int res[3][200];
-    int i1 = 0, i2 = 0, k = 0;
-    while(i1 < n1 && i2 < n2) {
-        int r1_pos = mat1[0][i1];
-        int c1_pos = mat1[1][i1];
-        int r2_pos = mat2[0][i2];
-        int c2_pos = mat2[1][i2];
-        if (r1_pos < r2_pos || (r1_pos == r2_pos && c1_pos < c2_pos)) {
-            res[0][k] = r1_pos;
-            res[1][k] = c1_pos;
-            res[2][k] = mat1[2][i1];
-            i1++; k++;
-        } else if (r2_pos < r1_pos || (r2_pos == r1_pos && c2_pos < c1_pos)) {
-            res[0][k] = r2_pos;
-            res[1][k] = c2_pos;
-            res[2][k] = mat2[2][i2];
-            i2++; k++;
-        } else {
-            int summed = mat1[2][i1] + mat2[2][i2];
-            if (summed != 0) { 
-                res[0][k] = r1_pos;
-                res[1][k] = c1_pos;
-                res[2][k] = summed;
-                k++;
-            }
-            i1++; i2++;
+#define MAX 100
+
+void readspars(int spars[MAX][MAX], int rows, int cols) {
+    int i, j;
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            scanf("%d", &spars[i][j]);
         }
     }
-    while(i1 < n1) {
-        res[0][k] = mat1[0][i1];
-        res[1][k] = mat1[1][i1];
-        res[2][k] = mat1[2][i1];
-        i1++; k++;
+}
+
+void sparsMatrix(int matrix[MAX][3], int rows, int cols, int spars[MAX][MAX]) {
+    int i, j, k = 1;  
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            if(spars[i][j] != 0) {
+                matrix[k][0] = i;
+                matrix[k][1] = j;
+                matrix[k][2] = spars[i][j];
+                k++;
+            }
+        }
     }
-    while(i2 < n2) {
-        res[0][k] = mat2[0][i2];
-        res[1][k] = mat2[1][i2];
-        res[2][k] = mat2[2][i2];
-        i2++; k++;
+    matrix[0][0] = rows;
+    matrix[0][1] = cols;
+    matrix[0][2] = k - 1;  
+}
+
+void displaymatrix(int matrix[MAX][3], int rows) {
+    int i, j;
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < 3; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
     }
-    printf("\nSum of Sparse Matrices (row column value):\n");
-    for(i = 0; i < k; i++) {
-        printf("%d %d %d\n", res[0][i], res[1][i], res[2][i]);
+}
+
+void addSparse(int a[MAX][3], int b[MAX][3], int result[MAX][3]) {
+    int i = 1, j = 1, k = 1;
+    if (a[0][0] != b[0][0] || a[0][1] != b[0][1]) {
+        printf("Matrix dimensions do not match for addition.\n");
+        result[0][2] = 0;
+        return;
     }
+    
+    result[0][0] = a[0][0];
+    result[0][1] = a[0][1];
+
+    while(i <= a[0][2] && j <= b[0][2]) {
+        if(a[i][0] < b[j][0] || (a[i][0] == b[j][0] && a[i][1] < b[j][1])) {
+            result[k][0] = a[i][0];
+            result[k][1] = a[i][1];
+            result[k][2] = a[i][2];
+            i++; k++;
+        }
+        else if(b[j][0] < a[i][0] || (b[j][0] == a[i][0] && b[j][1] < a[i][1])) {
+            result[k][0] = b[j][0];
+            result[k][1] = b[j][1];
+            result[k][2] = b[j][2];
+            j++; k++;
+        }
+        else {
+            int summed = a[i][2] + b[j][2];
+            if (summed != 0) {
+                result[k][0] = a[i][0];
+                result[k][1] = a[i][1];
+                result[k][2] = summed;
+                k++;
+            }
+            i++; j++;
+        }
+    }
+
+    while(i <= a[0][2]) {
+        result[k][0] = a[i][0];
+        result[k][1] = a[i][1];
+        result[k][2] = a[i][2];
+        i++; k++;
+    }
+
+    while(j <= b[0][2]) {
+        result[k][0] = b[j][0];
+        result[k][1] = b[j][1];
+        result[k][2] = b[j][2];
+        j++; k++;
+    }
+
+    result[0][2] = k - 1;
+}
+
+int main() {
+    int spars1[MAX][MAX], spars2[MAX][MAX];
+    int matrix1[MAX][3], matrix2[MAX][3], result[MAX][3];
+    int rows, cols;
+
+    printf("Enter the number of rows of sparse matrices: ");
+    scanf("%d", &rows);
+
+    printf("Enter the number of columns of sparse matrices: ");
+    scanf("%d", &cols);
+
+    printf("Enter the elements of the first sparse matrix:\n");
+    readspars(spars1, rows, cols);
+
+    printf("Enter the elements of the second sparse matrix:\n");
+    readspars(spars2, rows, cols);
+
+    sparsMatrix(matrix1, rows, cols, spars1);
+    sparsMatrix(matrix2, rows, cols, spars2);
+
+    addSparse(matrix1, matrix2, result);
+
+    printf("Resultant sparse matrix after addition:\n");
+    displaymatrix(result, result[0][2] + 1);
+
     return 0;
 }
